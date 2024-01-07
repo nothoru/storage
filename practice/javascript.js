58543045b6e446e3a821f27e32dd6f5edae18d2b
nothoru <yuiishi022@gmail.com>
2024-01-07 16:05:06 +0800

    start of learning js
    i've learned:
    variable
    how to declare variable - let(modern), var(old), const(unchanging value)
    variable naming - what's allowed and how a good naming can make it easier to read
    Numbers - are all floating based type unlike whether its a number with decimal or not
    Different Arithmetic operators:
    + addition, - subtraction, * multiplication, / division, % remainder,
    ** exponentation, ++ incrementation, --decrement

---
+++ practice/javascript.js
@@ -0,0 +1,23 @@
+
+let usingLet = "Hi";
+var usingVar =  "World";
+const usingConst = "Destroyed!";
+
+//Different arithmetic operators
+let a =  1 + 1;
+let b = 2 - 1;
+let c = 3 * 2;
+let d = 12 / 3;
+let e = 12 % 4;
+let f = ++a; //increment  result 3
+let g = --c; //decrement result 5
+let h = 5 ** 2; //exponentation results in 25
+let i = 10 + 2 * 5; //operator precedence PEMDAS
+
+
+
+
+
+
+
+
